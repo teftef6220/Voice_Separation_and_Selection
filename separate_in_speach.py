552fec0a06718cf60cbf4f8ab471953f5fd2d487
ddPn08 <pyn.g.1125@gmail.com>
2023-04-09 19:58:07 +0900

    Add `keep_silence` option

--- separate_in_speach.py
+++ separate_in_speach.py
@@ -20,6 +20,8 @@ parser.add_argument( '--min', type=int,required=False, default=2000,
                     help='min_chunk_len')
 parser.add_argument( '--freq', type=int,required=False, default=-40,
                     help='frequency')
+parser.add_argument( '--keep_silence', type=int, required=False, default=500,
+                    help='keep_silence')
 args = parser.parse_args()
 
 print("Running...")
@@ -40,7 +42,7 @@ min_silence_len = 1000  # 無音が1秒以上続いたら区切る
 silence_thresh = args.freq  # 音量が-40dB以下を無音と判定
 
 # 無音の部分で区切る
-chunks = split_on_silence(audio, min_silence_len=min_silence_len, silence_thresh=silence_thresh)
+chunks = split_on_silence(audio, min_silence_len=min_silence_len, silence_thresh=silence_thresh, keep_silence=args.keep_silence)
 
 # 出力ディレクトリがなければ作成
 if not os.path.exists(output_dir):
@@ -60,7 +62,6 @@ for chunk in chunks:
                 output_chunks.extend(sub_chunks[0:-1])
             else :
                 output_chunks.extend(sub_chunks)
-            
         else:
             output_chunks.append(chunk)
     else:
